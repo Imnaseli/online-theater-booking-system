f2f74a9771f0c9cadcb79065cfa9802e3411e5ba
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-11-28 16:13:04 +0100

    Edit name of static folder

--- theatherbooking/theatherbooking/settings.py
+++ theatherbooking/theatherbooking/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/styles/"
 
 STATICFILES_DIRS = [
         BASE_DIR /  'styles' , 
