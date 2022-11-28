3716d459d07fc575074e8c509c7771f375a8485f
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-11-28 12:18:44 +0100

    Build: Write rules for loading static css files

--- theatherbooking/theatherbooking/settings.py
+++ theatherbooking/theatherbooking/settings.py
@@ -123,3 +123,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# import os module
+# import os
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
